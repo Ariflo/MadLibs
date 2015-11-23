3688ee2566f39d3377298bb7251674dd18779e04
AriFlo <ariflo@arianflores.com>
2015-11-22 19:07:45 -0800

    touched up CSS of front page

--- js/MadLib.js
+++ js/MadLib.js
@@ -1,45 +1,99 @@
 var storyWords = [];
 var wordCount = 0;
-var fadeCount = 0; 
 var blankNum = 1;
 var example; 
 var word; 
 
 window.onload = function(){
 
+setInterval(function(){$("#header2").fadeOut();}, 1500);
+
+$("#header3").hide();
+
+setTimeout(function(){
+$("#header3").fadeIn();
+
+  setInterval(function(){$("#header3").fadeOut();},2000);
+}, 3000); 
+
+$("#header").hide();
+setInterval(function(){
+
+  $("#header").fadeIn('slow');
+  $('.StoryBox').slideDown('slow'); 
+  $('.StoryBox2').slideDown('slow'); 
+  $('.StoryBox3').slideDown('slow'); 
+  $('.StoryBox4').slideDown('slow'); 
+
+}, 6000); 
+
+$('.StoryBox').hide();
+$('.StoryBox2').hide();
+$('.StoryBox3').hide();
+$('.StoryBox4').hide();
+
+
+$('.StoryBox').on("mouseenter", function(){
+
+      $(this).add('#StoryBoxFocus');               
+});
+
+$('.StoryBox2').on("mouseenter", function(){
+
+       $(this).add('#StoryBoxFocus');                 
+});
+
+$('.StoryBox3').on("mouseenter", function(){
+
+       $(this).add('#StoryBoxFocus');              
+});
+
+$('.StoryBox4').on("mouseenter", function(){
+
+       $(this).add('#StoryBoxFocus');                
+});
+
 //push story selection to storySelect
 storyPusher();
 
+var choosenStory = new Story ();
+choosenStory.pasteStory(storyPusher()); 
+
+$("#storyBoard").append(choosenStory.story);
+$("#storyBoard").hide(); 
+
+$("#blank0").clone().appendTo($("#headTag"));
 
 $('#wordForm').on('submit', function(evt){
   evt.preventDefault();
-  var choosenStory = new Story ();
 
-        if (storyWords.length === wordCount){
+  word = $('#wordPush').val();
+  storyWords.push(word);
+
+  $('#wordForm').fadeOut('slow', function(){ 
+    example = $("#blank" + blankNum);
+    blankNum++;   
+                  
+  if(storyWords.length < wordCount){
+      $('#wordPush').val(' ');
+      $('#wordForm').fadeIn();   
+
+      $("#headTag").empty();
+      example.clone().appendTo($("#headTag"));
+
+    }else{
+
+      $('#pic1').remove();
+      $('#page2img').append('<img class="headerPic" src=" images/Story_logo.jpg">');  
+
+      $('#wordForm').hide();
+      fillInTheBlanks();
+
+      $("#storyBoard").show();
+    }  
+});
+
       
-                choosenStory.pasteStory(storyPusher());   
-                $('#pic1').remove();
-
-                $('#page2img').append('<img class="headerPic" src=" images/Story_logo.jpg">');  
-                $('#wordForm').hide();
-
-                $("#storyBoard").append(choosenStory.story);
-                fillInTheBlanks();
-
-        }else{
-                word = $('#wordPush').val();
-                storyWords.push(word);
-
-                $('#wordForm').fadeOut('slow', function(){ 
-                    fadeCount++;
-
-                    if(fadeCount <= wordCount){
-                      $('#wordForm').fadeIn();  
-
-                    }
-                });
-        }
-        
 });
 
 };
@@ -50,18 +104,14 @@ function storyPusher() {
   var queryString = document.location.search.replace('?', '');
   var choice = queryString.split('.');
   numOfBlanks(choice[0]);
+
   return choice[0];
-  
 };
 
 //fill in blanks in story 
 function fillInTheBlanks () {
 
       for(var i = 0; i < storyWords.length; i++){
-
-        //example = $("#blank" + blankNum);
-        //$("#wordPush").attr('placeholder', example);
-        //blankNum++;
 
         $("#blank" + i).empty();  
         $("#blank" + i).append(storyWords[i].replace("+", " "));  
